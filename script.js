b2a3b110ab1cdb12372f197e2ff438eec780bd4f
Bbaranoski <breno.b.o.baranoski@gmail.com>
2023-07-07 18:34:37 -0300

    make the read check

--- script.js
+++ script.js
@@ -1,39 +1,51 @@
-let myLibary = [];
+let myLibrary = [];
 
 let id = 0
 
-function Book(name, author, pages, id){
+function Book(name, author, pages, id, read){
      this.name = name
      this.author = author
      this.pages = pages
      this.id = id
+     this.read = read
 }
 
 function addBookToLibary(bookName){
-     myLibary.push(bookName)
+     myLibrary.push(bookName)
      createDom()
 }
 
 function createDom(){
-     myLibary.forEach((obj) => {
+     myLibrary.forEach((obj) => {
           if(obj.id == id){
                const Div = document.createElement('div')
                const Pname = document.createElement('p')
                const Pauthor = document.createElement('p')
                const Ppages = document.createElement('p')
+               const ReadDiv = document.createElement('div')
+               const check = document.createElement('input')
+               const label = document.createElement('label')
                const remove = document.createElement('button')
                Pname.textContent = `Name: ${obj.name}`
                Pauthor.textContent = `Author: ${obj.author}`
                Ppages.textContent = `Pages: ${obj.pages}`
+               label.textContent = 'Read:'
                remove.textContent = 'REMOVE'
                Div.appendChild(Pname)
                Div.appendChild(Pauthor)
                Div.appendChild(Ppages)
+               ReadDiv.appendChild(label)
+               ReadDiv.appendChild(check)
+               Div.appendChild(ReadDiv)
                Div.appendChild(remove)
                book.appendChild(Div)
                Div.setAttribute('id', `${obj.id}`)
+               check.setAttribute('type', 'checkbox')
+               check.setAttribute('id', 'check')
+               label.setAttribute('for', 'check')
                remove.setAttribute('class', 'remove')
                remove.setAttribute('id', `${obj.id}`)
+               if(obj.read == true){check.checked = true}
      }});
      book.appendChild(buttonForm)
 }
@@ -57,11 +69,12 @@ button.addEventListener('click', () => {
      name.value = ''
      author.value = ''
      pages.value = ''
+     read.checked = false
 })
 
 form.addEventListener('submit', (e) => {
      e.preventDefault()
-     const book = new Book(name.value, author.value, pages.value, id)
+     const book = new Book(name.value, author.value, pages.value, id, read.checked)
      addBookToLibary(book)
      forms.remove()
 
@@ -74,6 +87,6 @@ form.addEventListener('submit', (e) => {
                     removeItem.remove()
           })}
      })
-
+     
      id += 1
 })
